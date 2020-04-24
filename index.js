0baecba7e72137324b016a338ca6298c88f2c6ea
Bilge <lagala99@gmail.com>
2020-04-24 23:34:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let multiply;
+let num1= 31;
+let num2= 2;
+multiply = num1*num2;
+
+let random = Math.random()+1;
+
+let mod = 12%8;
+
+let max = Math.max(1,17,6,3,4,8,20);
+  
